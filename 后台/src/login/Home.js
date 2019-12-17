8a52a8567cac0fe24053a36cec4ac94781cd365a
houyuqin <1738519809@qq.com>
2019-12-17 18:31:25 +0800

    add 后台

---
+++ 后台/src/login/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../index.css'
+
+export default class Todoinput extends Component {
+    render() {
+        return (
+            <div style={{width:'100%',height:'100%'}}>
+                <div style={{width:'30%',height:'100%',backgroundColor:'#ff9900'}}>sadsad</div>
+                <div style={{width:'70%',height:'100%',backgroundColor:'white'}}></div>
+            </div>
+        )
+    }
+}
