1c4566b13530c95843f631e1a48bb6de743bc511
houyuqin <1738519809@qq.com>
2019-12-18 09:23:51 +0800

    后台 Home

--- 后台/src/login/Home.js
+++ 后台/src/login/Home.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react'
 import '../index.css'
 
 export default class Todoinput extends Component {
+    one=()=>{
+        window.location = "http://localhost:8000/tearcm"
+    }
+    two=()=>{
+        window.location = "http://localhost:8000/vedio"
+    }
+    three=()=>{
+        window.location = "http://localhost:8000/question"
+    }
     render() {
         return (
             <div style={{width:'100%',height:'100%'}}>
-                <div style={{width:'30%',height:'100%',backgroundColor:'#ff9900'}}>sadsad</div>
-                <div style={{width:'70%',height:'100%',backgroundColor:'white'}}></div>
+                <div style={{width:'20%',height:'100%',backgroundColor:'#ff9900',float:'left'}}>
+                    <ul>
+                        <li style={{height:50,border:'1px solid blue',fontSize:30,textAlign:'center'}}
+                        >
+                            <a href="#" onClick={this.one} style={{color:'blue',textDecoration:'none'}}>教师推荐</a>
+                        </li>
+                        <li style={{height:50,border:'1px solid blue',fontSize:30,textAlign:'center'}}
+                        >
+                            <a href="#" onClick={this.two} style={{color:'blue',textDecoration:'none'}}>视频推荐</a>
+                        </li>
+                        <li style={{height:50,border:'1px solid blue',fontSize:30,textAlign:'center'}}
+                        >
+                            <a href="#" onClick={this.three} style={{color:'blue',textDecoration:'none'}}>问卷情况</a>
+                        </li>
+                    </ul>
+                </div>
+                <div style={{width:'80%',height:'100%',backgroundColor:'red',float:'left'}}>
+                    
+                </div>
             </div>
         )
     }
