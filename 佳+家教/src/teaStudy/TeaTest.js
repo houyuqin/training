71e5c213795282d70a36fec2a0fbf10c3babcda6
zhouxuan2017 <1207469303@qq.com>
2019-12-19 22:35:44 +0800

    time

--- 佳+家教/src/teaStudy/TeaTest.js
+++ 佳+家教/src/teaStudy/TeaTest.js
@@ -6,18 +6,36 @@ export default class Teatest extends Component {
     constructor(){
         super();
         this.state={
-            data:[]
+            data:[],
+            name:'',
+            time:''
         }
+    }
+    componentDidMount(){
+        var me=window.location.search.split('=')[1];
+        let today = new Date(),
+        time0 = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+        +'   '+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
+        this.setState({
+            time:time0
+        });
+        fetch(`http://148.70.183.184:8006/teamine/${me}`)
+        .then(res=>res.json())
+        .then(res=>{
+            this.setState({
+                name:res.data[0].wusername
+            })
+        })
     }
     fabu=()=>{
         var a={};
-
         a.kemu=this.cren1.value;
         a.title=this.cren2.value;
-        a.time=this.cren3.value;
-        a.author=this.cren4.value;
+        a.time=this.state.time;
+        a.author=this.state.name;
         a.content=this.cren5.value;
         a.userphone=window.location.search.split('=')[1]
+        console.log(a);
     
         fetch("http://148.70.183.184:8005/taskt", {
             method: "POST",
@@ -26,12 +44,13 @@ export default class Teatest extends Component {
             },
             body: JSON.stringify(a)
           }).then(function(response) {
-            // do sth
-           alert('您的任务发布完毕！')
-        //   }})
+            alert('您的任务发布完毕！')
           })
     }
     render() {
+    let today = new Date(),
+    time = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+        +'   '+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
         return (
             <div className='cbu'>
                 <NavBar
@@ -48,18 +67,16 @@ export default class Teatest extends Component {
                         <input type="text" name="task" className="cren1" ref={i=>this.cren2=i}></input>
                     </div>
                     <div className="cbu4">   
-                        发布时间:
-                        <input type="text" name="task" className="cren1" ref={i=>this.cren3=i}></input>
+                        发布时间:{time}   
                     </div>
                     <div className="cbu4">   
-                        发布人:
-                        <input type="text" name="task" className="cren1" ref={i=>this.cren4=i}></input>
+                        发布人:{this.state.name}
                     </div>
                     <div className="cbu44">   
                         内容:
                         <textarea name="task" cols="40" rows="5" className="cren2" ref={i=>this.cren5=i}></textarea>      
                     </div>
-                    <input type="submit" value="发布" className="cbuan"  onClick={()=>this.fabu()}/>
+                    <input type="submit" value="发布" className="cbuan"  onClick={()=>this.fabu(time)}/>
                 </div>
             </div>
         )
