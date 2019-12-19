b37eaddd9a1d78ac31420397b893b64cadcd1678
houyuqin <1738519809@qq.com>
2019-12-19 11:17:07 +0800

    modify tongzhi

--- 佳+家教/src/stdMine/container/tongzhi.js
+++ 佳+家教/src/stdMine/container/tongzhi.js
@@ -1,29 +1,84 @@
 import React, { Component } from 'react'
 import { NavBar ,Icon} from 'antd-mobile';
-import { Link } from 'react-router-dom';
+import logo from "../../img/caoqian.png"
 
 export default class tongzhi extends Component {
     constructor(){
         super();
         this.state={
-            data:['未收到任何消息','语文作业提醒','老师回复','作业发布提醒','新上任老师查看','线上视频新发布','广告推荐','有关双十二来临课程折扣','已选课程情况','您购买的课程已过期']
+            data:[{content:'未收到任何消息'}]
         }
+    }
+    componentDidMount(){
+        fetch('http://148.70.183.184:8000/inforuser')
+        .then(res=>res.json())
+        .then(res=>{
+            this.setState({
+                data:res.data
+            })
+        })
+    }
+    componentDidUpdate(){
+        fetch('http://148.70.183.184:8000/inforuser')
+        .then(res=>res.json())
+        .then(res=>{
+            this.setState({
+                data:res.data
+            })
+        })
+    }
+    rtn = ()=>{
+        this.props.history.push('/');
+    };
+    del=(id0)=>{
+        let a={id:id0}
+        fetch('http://148.70.183.184:8000/inforuserdel',{
+            method:"DELETE",
+            headers:{
+                'Accept':'application/json',
+                'Content-Type': 'text/plain',
+            },
+            body:JSON.stringify(a)
+        })
+        .then((res)=>{ 
+            if(res.status === 200){
+                return res.json();
+            }else{
+                
+            }
+        })
+        .then((data)=>{
+            console.log(data);
+        }).catch((err)=>{
+            console.log(err);
+        });
     }
     render() {
         return (
-            <div>
+            <div style={{width:'100%',height:'100%',background:`url(${logo}) center center / cover no-repeat`,overflow:'scroll'}}>
                 <NavBar
-                style={{backgroundColor:'white',color:'black'}}
-                icon={<Link to='/'><Icon style={{color:'black'}} type="left" /></Link>}
-                >消息</NavBar>
+                mode="dark"
+                icon={<Icon style={{color:'black'}} type="left" 
+                onClick={this.rtn}/>}
+                >消息通知</NavBar>
 
                 {
-                   this.state.data.map((item,index)=>(
-                    <div style={{width:'100%',height:'70px',borderBottom:'2px solid white'}} ><span className='zmess'>{item}</span>
-                    <div style={{float:'right',height:'30px',width:'50px',marginRight:'25px',marginTop:'20px',color:'white'}}><img src='./img/3.png' style={{width:'20px',height:'20px'}}></img></div></div>
-                   ))
-               }
+                    this.state.data.map((item)=>(
+                        <div 
+                        key={item}
+                        style={{width:'100%',borderBottom:'1px solid black',fontSize:20,float:'left'}}>
+                            {item.content}
+                            
+                            <button onClick={()=>this.del(item.id)}
+                            style={{backgroundColor:'rgb(31, 138, 238)',color:'white',float:'right',marginRight:5}}
+                            >
+                                已读删除
+                            </button>
+                        </div>
+                    ))
+                }
 
+                
             </div>
         )
     }
